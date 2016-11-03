ac6ef0c54fc5ce3b2f90f40c3a260b33908996cd
jeromeCabridens <jcabridens@rennes.sii.fr>
2016-11-03 14:15:18 +0100

    load availability

---
+++ src/BYS/CoreBundle/DataFixtures/LoadAvailability.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jcabridens
+ * Date: 03/11/2016
+ * Time: 11:54
+ */
+
+namespace BYS\CoreBundle\DataFixtures;
+
+
+use BYS\CoreBundle\Entity\Availability;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadAvailability implements FixtureInterface
+{
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $faker = \Faker\Factory::create();
+
+        for ($i=0; $i<10; $i++){
+            $availability = new Availability();
+            $availability->setDate($faker->date($format = 'Y-m-d', $max = 'now'));
+            $availability->setIsPresent($faker->bool);
+            $availability->setFreeFrom($faker->regexify('1[1-3]h[1-9][1-9]'));
+            $availability->setMembership($this->getReference('membership').rand(0,10));
+
+            $manager->persist($availability);
+
+            $this->addReference('availability'.$i, $availability);
+        }
+        $manager->flush();
+
+
+    }
+
+    public function getOrder(){
+        return 17;
+    }
+}
